1d895501c802a07de99fabad0fd46ebe04cc65c4
Tomas Sardyha <darsain@gmail.com>
2015-03-07 15:12:30 +0100

    Formatting.

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-'use strict';
-
 /**
  * Bind `el` event `type` to `fn`.
  *
  * @param {Element}  el
  * @param {String}   type
  * @param {Function} fn
- * @param {Boolean}  capture
+ * @param {Boolean}  [capture]
  *
- * @return {Function}
+ * @return {Function} `fn`
  */
 exports.bind = window.addEventListener ? function (el, type, fn, capture) {
 	el.addEventListener(type, fn, capture || false);
 	return fn;
 } : function (el, type, fn) {
 	var fnid = type + fn;
+
 	el[fnid] = el[fnid] || function () {
 		var event = window.event;
 		event.target = event.srcElement;
@@ -22,6 +21,7 @@ exports.bind = window.addEventListener ? function (el, type, fn, capture) {
 		event.stopPropagation = stopPropagation;
 		fn.call(el, event);
 	};
+
 	el.attachEvent('on' + type, el[fnid]);
 	return fn;
 };
@@ -32,9 +32,9 @@ exports.bind = window.addEventListener ? function (el, type, fn, capture) {
  * @param {Element}  el
  * @param {String}   type
  * @param {Function} fn
- * @param {Boolean}  capture
+ * @param {Boolean}  [capture]
  *
- * @return {Function}
+ * @return {Function} `fn`
  */
 exports.unbind = window.removeEventListener ? function (el, type, fn, capture) {
 	el.removeEventListener(type, fn, capture || false);
@@ -42,12 +42,15 @@ exports.unbind = window.removeEventListener ? function (el, type, fn, capture) {
 } : function (el, type, fn) {
 	var fnid = type + fn;
 	el.detachEvent('on' + type, el[fnid]);
+
+	// clean up reference to handler function, but with a fallback
+	// because we can't delete window object properties
 	try {
 		delete el[fnid];
 	} catch (err) {
-		// can't delete window object properties
 		el[fnid] = undefined;
 	}
+
 	return fn;
 };
 
